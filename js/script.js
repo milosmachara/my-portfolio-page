60067d3e42bd349a4a73a4d328b334c9daffa415
“MilosMachara“ <milosmachara@gmail.com>
2025-10-25 10:08:28 +0200

    Pridal som skills SQL a GIT

--- js/script.js
+++ js/script.js
@@ -3,10 +3,3 @@ $(function () {
     window.location.href = "index.html"; // Presmerovanie na domovskú stránku
   });
 });
-window.addEventListener("scroll", () => {
-  const mainImg = document.querySelector(".main-img img");
-  let scrollY = window.scrollY;
-  
-  // rýchlosť efektu – môžeš doladiť (0.3 = jemný pohyb)
-  mainImg.style.transform = `translateY(${scrollY * 0.3}px)`;
-});
