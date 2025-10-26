c18170b559e84c4e476ea002ed5b18cf3cfdd39a
“MilosMachara“ <milosmachara@gmail.com>
2025-10-26 11:57:39 +0100

    pridany fade in na skills

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ $(function () {
     window.location.href = "index.html"; // Presmerovanie na domovskú stránku
   });
 });
+
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = { threshold: 0.1 };
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => appearOnScroll.observe(fader));
